f045c49be568db084daf33ea4e1ad5d447a0b4ba
andjelkadzida <andjelkadzida@gmail.com>
2021-05-09 15:01:13 +0200

    Added tests for controller and two models.

---
+++ src/test/java/projekat/bioskop/controller/FilmControllerTest.java
@@ -0,0 +1,29 @@
+package projekat.bioskop.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmControllerTest
+{
+
+    @Test
+    void spisakFilmova()
+    {
+    }
+
+    @Test
+    void spisakProjekcija()
+    {
+    }
+
+    @Test
+    void izaberiSedista()
+    {
+    }
+
+    @Test
+    void izabranaSedista()
+    {
+    }
+}
