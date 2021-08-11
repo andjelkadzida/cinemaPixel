1fd836c184ecc02aedf0d06bdc5b5210078cd491
Andjelka Dzida <35188857+andjelkadzida@users.noreply.github.com>
2021-08-11 23:12:47 +0200

    RegistrationControllerTest.java mostly completed FilmControllerTest.java to be finished one day

--- src/test/java/projekat/bioskop/controller/FilmControllerTest.java
+++ src/test/java/projekat/bioskop/controller/FilmControllerTest.java
@@ -1,23 +1,18 @@
 package projekat.bioskop.controller;
 
 import java.util.*;
-import javax.mail.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import projekat.bioskop.model.Film;
 import projekat.bioskop.model.Projekcija;
 import projekat.bioskop.repository.KorisnikRepository;
 import projekat.bioskop.repository.ProjekcijaRepository;
@@ -25,15 +20,14 @@ import projekat.bioskop.repository.RezervacijaRepository;
 import projekat.bioskop.repository.RezervisanaSedistaRepository;
 import projekat.bioskop.repository.SedisteRepository;
 import projekat.bioskop.services.FilmService;
+import projekat.bioskop.services.SalaService;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ContextConfiguration(classes = {FilmController.class})
 @ExtendWith(SpringExtension.class)
 class FilmControllerTest
 {
-
     @Autowired
     private FilmController filmController;
 
@@ -57,86 +51,6 @@ class FilmControllerTest
 
     @MockBean
     private SedisteRepository sedisteRepository;
-
-    @Test
-    public void testConstructor() throws NoSuchProviderException
-    {
-        JavaMailSenderImpl javaMailSenderImpl = new JavaMailSenderImpl();
-        FilmController actualFilmController = new FilmController(javaMailSenderImpl);
-        assertNull(actualFilmController.filmService);
-        assertNull(actualFilmController.sedisteRepository);
-        assertNull(actualFilmController.rezervisanaSedistaRepository);
-        assertNull(actualFilmController.rezervacijaRepository);
-        assertNull(actualFilmController.projekcijaRepository);
-        assertNull(actualFilmController.korisnikRepository);
-        JavaMailSender javaMailSender = actualFilmController.javaMailSender;
-        assertTrue(javaMailSender instanceof JavaMailSenderImpl);
-        assertNull(((JavaMailSenderImpl) javaMailSender).getDefaultEncoding());
-        assertNull(((JavaMailSenderImpl) javaMailSender).getUsername());
-        assertNull(((JavaMailSenderImpl) javaMailSender).getProtocol());
-        assertTrue(((JavaMailSenderImpl) javaMailSender)
-                .getDefaultFileTypeMap() instanceof org.springframework.mail.javamail.ConfigurableMimeFileTypeMap);
-        Properties javaMailProperties = ((JavaMailSenderImpl) javaMailSender).getJavaMailProperties();
-        assertTrue(javaMailProperties.isEmpty());
-        assertNull(((JavaMailSenderImpl) javaMailSender).getPassword());
-        assertNull(((JavaMailSenderImpl) javaMailSender).getHost());
-        assertEquals(-1, ((JavaMailSenderImpl) javaMailSender).getPort());
-        Session session = ((JavaMailSenderImpl) javaMailSender).getSession();
-        assertEquals(12, session.getProviders().length);
-        assertFalse(session.getDebug());
-        assertSame(javaMailProperties, session.getProperties());
-        Transport transport = session.getTransport();
-        assertFalse(transport.isConnected());
-        String expectedToStringResult = String.join("", "smtp://", System.getProperty("user.name"), "@");
-        assertEquals(expectedToStringResult, transport.toString());
-        String expectedToStringResult1 = String.join("", "smtp://", System.getProperty("user.name"), "@");
-        URLName uRLName = transport.getURLName();
-        assertEquals(expectedToStringResult1, uRLName.toString());
-        String expectedUsername = System.getProperty("user.name");
-        assertEquals(expectedUsername, uRLName.getUsername());
-        assertNull(uRLName.getRef());
-        assertEquals("smtp", uRLName.getProtocol());
-        assertEquals(-1, uRLName.getPort());
-        assertNull(uRLName.getPassword());
-        assertNull(uRLName.getHost());
-        assertNull(uRLName.getFile());
-        assertSame(javaMailSender, javaMailSenderImpl);
-    }
-
-    @Test
-    public void testIzabranaSedista() throws Exception
-    {
-        MockHttpServletRequestBuilder postResult = MockMvcRequestBuilders.post("/selektovanaSedista");
-        MockHttpServletRequestBuilder paramResult = postResult.param("projekcijaId", String.valueOf(1L));
-        MockHttpServletRequestBuilder requestBuilder = paramResult.param("sediste", String.valueOf(new HashSet<Long>()));
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(this.filmController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(400));
-    }
-
-    @Test
-    public void testIzabranaSedista2() throws Exception
-    {
-        MockHttpServletRequestBuilder postResult = MockMvcRequestBuilders.post("/selektovanaSedista");
-        MockHttpServletRequestBuilder requestBuilder = postResult.param("projekcijaId", String.valueOf(1L))
-                .param("sediste", "https://example.org/example");
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(this.filmController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(400));
-    }
-
-    @Test
-    public void testSpisakFilmova() throws Exception
-    {
-        when(this.filmService.sviFilmovi()).thenReturn(new ArrayList<Film>());
-        SecurityMockMvcRequestBuilders.FormLoginRequestBuilder requestBuilder = SecurityMockMvcRequestBuilders.formLogin();
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(this.filmController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
-    }
 
     @Test
     public void testSpisakProjekcija() throws Exception
--- src/test/java/projekat/bioskop/controller/RegistrationControllerTest.java
+++ src/test/java/projekat/bioskop/controller/RegistrationControllerTest.java
@@ -1,19 +1,71 @@
 package projekat.bioskop.controller;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+import projekat.bioskop.model.Korisnik;
+import projekat.bioskop.services.KorisnikService;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
+@ContextConfiguration(classes = {RegistrationController.class})
+@ExtendWith(SpringExtension.class)
 class RegistrationControllerTest
 {
 
+    @MockBean
+    private KorisnikService korisnikService;
+
+    @Autowired
+    private RegistrationController registrationController;
+
     @Test
-    void registracija()
+    public void testModelAndViewRegistracija() throws Exception
     {
+        RegistrationController registrationController = new RegistrationController();
+        ModelAndView modelAndView = registrationController.registracija();
+        Assertions.assertEquals("registracija", modelAndView.getViewName());
     }
 
     @Test
-    void registracijaKorisnika()
+    public void testRegistracija() throws Exception
     {
+        SecurityMockMvcRequestBuilders.FormLoginRequestBuilder requestBuilder = SecurityMockMvcRequestBuilders.formLogin();
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(this.registrationController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
     }
+
+    @Test
+    public void testRegistracijaKorisnika() throws Exception
+    {
+        doNothing().when(this.korisnikService).sacuvajKorisnika((projekat.bioskop.model.Korisnik) any());
+        when(this.korisnikService.postojiKorisnik((projekat.bioskop.model.Korisnik) any())).thenReturn(false);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/registracija");
+        MockMvcBuilders.standaloneSetup(this.registrationController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("korisnik"))
+                .andExpect(MockMvcResultMatchers.view().name("login"))
+                .andExpect(MockMvcResultMatchers.forwardedUrl("login"));
+    }
+
 }
