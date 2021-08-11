94e35708b687d03192fb8c5f239c2c4d9c8c0f92
andjelkadzida <andjelkadzida@gmail.com>
2021-08-11 21:10:04 +0200

    SalaServiceTest complete

--- src/test/java/projekat/bioskop/services/SalaServiceTest.java
+++ src/test/java/projekat/bioskop/services/SalaServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import projekat.bioskop.model.Sala;
 import projekat.bioskop.repository.SalaRepository;
 
